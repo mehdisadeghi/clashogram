bcdf18e6ff78e67ee276c85dd694e4d9b14c35d4
Mehdi Sadeghi <mehdi@mehdix.org>
2017-06-02 23:47:12 +0200

    Almost working with correct locate and jdatetime

--- clashogram.py
+++ clashogram.py
@@ -5,6 +5,12 @@ import click
 import json
 import shelve
 import requests
+import jdatetime
+import locale
+import pytz
+from dateutil.parser import parse as dateutil_parse
+
+locale.setlocale(locale.LC_ALL, "fa_IR")
 
 POLL_INTERVAL = 1
 
@@ -26,7 +32,9 @@ def monitor_currentwar(coc_token, clan_tag, bot_token, channel_name):
         telegram_updater = TelegramUpdater(db, bot_token, channel_name)
         while True:
             wardata = get_currentwar(coc_token, clan_tag)
+            print(wardata['state'])
             telegram_updater.update(wardata)
+            save_wardata(wardata)
             time.sleep(POLL_INTERVAL)
 
 
@@ -44,33 +52,66 @@ def get_currentwar_endpoint(clan_tag):
             clan_tag=requests.utils.quote('#%s' % clan_tag))
 
 
+def save_wardata(wardata):
+    if wardata['state'] != 'notInWar':
+        war_id = "{0}{1}".format(wardata['clan']['tag'][1:],
+                                 wardata['preparationStartTime'])
+        if not os.path.exists('warlog'):
+            os.mkdir('warlog')
+        path = os.path.join('warlog', war_id)
+        json.dump(wardata, open(path, 'w'), ensure_ascii=False)
+    
+
 class TelegramUpdater(object):
     def __init__(self, db, bot_token, channel_name):
         self.db = db
         self.bot_token = bot_token
         self.channel_name = channel_name
         self.latest_wardata = None
+        self.clan_members = {}
+        self.opponent_members = {}
+        self.players = {}
 
     def update(self, wardata):
-        self.latest_wardata = wardata
-        if self.get_war_id() not in self.db:
-            self.db[self.get_war_id()] = {}
+        if wardata['state'] == 'notInWar':
+            print('Not in war. Waiting.')
+            return
+
+        self.populate_warinfo(wardata)
         if self.is_in_preparation():
             self.send_preparation_msg()
         elif self.is_in_war():
             self.send_war_msg()
+            self.send_attack_msgs()
         elif self.is_war_over():
             self.send_war_over_msg()
         else:
             print("Current war status is uknown. We stay quiet.")
 
+    def populate_warinfo(self, wardata):
+        self.latest_wardata = wardata
+        if self.get_war_id() not in self.db:
+            self.db[self.get_war_id()] = {}
+        if self.is_new_war(wardata):
+            for member in wardata['clan']['members']:
+                self.clan_members[member['tag']] = member
+                self.players[member['tag']] = member
+            for opponent in wardata['opponent']['members']:
+                self.opponent_members[opponent['tag']] = opponent
+                self.players[opponent['tag']] = opponent
+
+    def is_new_war(self, wardata):
+        return self.create_war_id(wardata) in self.db
+
     def get_war_id(self):
-        return "{0}{1}".format(self.latest_wardata['clan']['tag'],
-                               self.latest_wardata['preparationStartTime'])
+        return self.create_war_id(self.latest_wardata)
+
+    def create_war_id(self, wardata):
+        return "{0}{1}".format(wardata['clan']['tag'],
+                               wardata['preparationStartTime'])
 
     def is_in_preparation(self):
-        print("state is %s" % self.latest_wardata['state'])
-        return self.latest_wardata['state'] == 'inPreparation'
+        return self.latest_wardata['state'] == 'preparation'
 
     def send_preparation_msg(self):
         if not self.is_preparation_msg_sent():
@@ -82,7 +123,27 @@ class TelegramUpdater(object):
         return self.db[self.get_war_id()].get('preparation_msg_sent', False)
 
     def create_preparation_msg(self):
-        return 'preparation msg'
+        msg_template = """{top_imoji} {title}
+کلن {ourclan} در برابر کلن {opponentclan}
+تگ {ourtag} در برابر {opponenttag}
+جنگ قبیله {start} شروع خواهد شد.
+شاد باشید! {final_emoji}
+"""
+        msg = msg_template.format(top_imoji='\U0001F3C1',
+                                  title='جنگ  در راه است!',
+                                  ourclan=self.latest_wardata['clan']['name'],
+                                  opponentclan=self.latest_wardata['opponent']['name'],
+                                  ourtag=self.latest_wardata['clan']['tag'],
+                                  opponenttag=self.latest_wardata['opponent']['tag'],
+                                  start=self.format_time(self.latest_wardata['endTime']),
+                                  final_emoji='\U0001F6E1')
+        return msg
+
+    def format_time(self, timestamp):
+        utc_time = dateutil_parse(timestamp, fuzzy=True)
+        tehran_time = utc_time.replace(tzinfo=pytz.timezone("Asia/Tehran"))
+        fmt = jdatetime.datetime.fromgregorian(datetime=tehran_time).strftime("%a, %d %b %Y %H:%M:%S")
+        return convert_to_persian_numbers(fmt)
 
     def is_in_war(self):
         return self.latest_wardata['state'] == 'inWar'
@@ -97,12 +158,110 @@ class TelegramUpdater(object):
         return self.db[self.get_war_id()].get('war_msg_sent', False)
 
     def create_war_msg(self):
-        return 'war message'
+        return 'war start message'
 
-    def is_war_over():
-        return self.latest_wardata['state'] == 'over'
+    def send_attack_msgs(self):
+        for member in self.clan_members.values():
+            if 'attacks' in member:
+                self.send_clan_attack_msg(member)
+        for opponent in self.opponent_members.values():
+            if 'attacks' in opponent:
+                self.send_opponent_attack_msg(opponent)
+    
+    def send_clan_attack_msg(self, attacker):
+        for attack in attacker['attacks']:
+            if not self.is_attack_msg_sent(attack):
+                msg = self.create_clan_attack_msg(attacker, attack)
+                self.send(msg)
+                self.db[self.get_war_id()][self.get_attack_id(attack)] = True
 
-    def send_war_over_msg():
+    def is_attack_msg_sent(self, attack):
+        attack_id = self.get_attack_id(attack)
+        return self.db[self.get_war_id()].get(attack_id, False)
+
+    def create_clan_attack_msg(self, member, attack):
+        msg_template = """{top_imoji} {title}
+کلن {ourclan}\tدر برابر\tکلن {opponentclan}
+تگ {ourtag}\tدر برابر\t{opponenttag}
+مهاجم:\t{attacker_name}\tتاون {attacker_thlevel} رده {attacker_map_position}
+در مصاف
+مدافع:\t{defender_name}\tتاون ${defender_thlevel} رده {defender_map_position}
+ستاره‌های قبلی:\t{previous_stars}\tستاره‌های جدید:\t{new_stars}
+درصد تخریب: {destruction_percentage}%
+
+شاد باشید!\t {final_emoji}
+"""
+        defender = self.get_player_info(attack['defenderTag'])
+        msg = msg_template.format(top_imoji='\U0001F535',
+                                  title='گزارش حمله! \U0001F692',
+                                  ourclan=self.latest_wardata['clan']['name'],
+                                  opponentclan=self.latest_wardata['opponent']['name'],
+                                  ourtag=self.latest_wardata['clan']['tag'],
+                                  opponenttag=self.latest_wardata['opponent']['tag'],
+                                  attacker_name=member['name'],
+                                  attacker_thlevel=member['townhallLevel'],
+                                  attacker_map_position=member['mapPosition'],
+                                  defender_name=defender['name'],
+                                  defender_thlevel=defender['townhallLevel'],
+                                  defender_map_position=defender['mapPosition'],
+                                  previous_stars='?',
+                                  new_stars=attack['stars'],
+                                  destruction_percentage=attack['destructionPercentage'],
+                                  final_emoji='\U0001F6E1')
+        return msg
+
+    def get_player_info(self, tag):
+        if tag not in self.players:
+            raise Exception('Player %s not found.' % tag)
+        return self.players[tag]
+
+    def get_attack_id(self, attack):
+        return "attack{}{}".format(attack['attackerTag'][1:],
+    
+                                   attack['defenderTag'][1:])
+
+    def send_opponent_attack_msg(self, attacker):
+        for attack in attacker['attacks']:
+            if not self.is_attack_msg_sent(attack):
+                msg = self.create_opponent_attack_msg(attacker, attack)
+                self.send(msg)
+                self.db[self.get_war_id()][self.get_attack_id(attack)] = True
+
+    def create_opponent_attack_msg(self, member, attack):
+        msg_template = """{top_imoji} {title}
+کلن {ourclan}\tدر برابر\tکلن {opponentclan}
+تگ {ourtag}\tدر برابر\t{opponenttag}
+مهاجم:\t{attacker_name}\tتاون {attacker_thlevel} رده {attacker_map_position}
+در مصاف
+مدافع:\t{defender_name}\tتاون ${defender_thlevel} رده {defender_map_position}
+ستاره‌های قبلی:\t{previous_stars}\tستاره‌های جدید:\t{new_stars}
+درصد تخریب: {destruction_percentage}%
+
+شاد باشید!\t {final_emoji}
+"""
+        defender = self.get_player_info(attack['defenderTag'])
+        msg = msg_template.format(top_imoji='\U0001F534',
+                                  title='کلن زیر آتش! \U0001F691',
+                                  ourclan=self.latest_wardata['clan']['name'],
+                                  opponentclan=self.latest_wardata['opponent']['name'],
+                                  ourtag=self.latest_wardata['clan']['tag'],
+                                  opponenttag=self.latest_wardata['opponent']['tag'],
+                                  attacker_name=member['name'],
+                                  attacker_thlevel=member['townhallLevel'],
+                                  attacker_map_position=member['mapPosition'],
+                                  defender_name=defender['name'],
+                                  defender_thlevel=defender['townhallLevel'],
+                                  defender_map_position=defender['mapPosition'],
+                                  previous_stars='?',
+                                  new_stars=attack['stars'],
+                                  destruction_percentage=attack['destructionPercentage'],
+                                  final_emoji='\U0001F6E1')
+        return msg
+
+    def is_war_over(self):
+        return self.latest_wardata['state'] == 'warEnded'
+
+    def send_war_over_msg(self):
         if not self.is_war_over_msg_sent():
             msg = self.create_war_over_msg()
             self.send(msg)
@@ -115,10 +274,21 @@ class TelegramUpdater(object):
         return 'war over message'
 
     def send(self, msg):
-        endpoint = "https://api.telegram.org/bot{bot_token}/sendMessage?parse_mode={mode}&chat_id=@{channel_name}&text={text}".format(bot_token=self.bot_token, mode='Markdown', channel_name=self.channel_name, text=msg)
+        print(msg)
+        endpoint = "https://api.telegram.org/bot{bot_token}/sendMessage?parse_mode={mode}&chat_id=@{channel_name}&text={text}".format(bot_token=self.bot_token, mode='Markdown', channel_name=self.channel_name, text=requests.utils.quote(msg))
+        print(endpoint)
         requests.post(endpoint)
 
 
+def convert_to_persian_numbers(text):
+    # Supper intelligent and super efficient :)
+    return text.replace('0', '۰').replace('1', '۱').replace('2', '۲').replace('3', '۳').replace('4', '۴').replace('5', '۵').replace('6', '۶').replace('7', '۷').replace('8', '۸').replace('9', '۹')
+
+
 if __name__ == '__main__':
-    main(auto_envvar_prefix='CLASHOGRAM')
+    try:
+        main()
+    except Exception as error:
+        print(error)
+        raise(error)
 

b3b64cd240a08175a4b386f6f29d21617b6c78c3
Mehdi Sadeghi <mehdi@mehdix.org>
2017-06-10 14:37:22 +0200

    Make the message even funnier!

--- clashogram.py
+++ clashogram.py
@@ -41,7 +41,7 @@ def monitor_currentwar(coc_token, clan_tag, bot_token, channel_name):
                 save_wardata(wardata)
                 time.sleep(POLL_INTERVAL)
             except:
-                telegram_updater.send("☠️ 😵 رئیس سرور ترکیده. با آدمتون تماس بگیرید درستش کنه.")
+                telegram_updater.send("☠️ 😵 رئیس من ترکیدم! با آدمتون تماس بگیرید بیاد درستم کنه. قربان شما، ربات. 🤖")
                 raise
 
 

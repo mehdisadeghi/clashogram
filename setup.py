3480413ba077eb2bb930d6caa1a9bbb3ea8022a4
Mehdi Sadeghi <mehdi@mehdix.org>
2017-08-01 19:39:11 +0200

    Add 0.1.8

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='Clashogram',
-      version='0.1.6',
+      version='0.1.8',
       description='Clash of Clans war moniting for telegram channels.',
       author='Mehdi Sadeghi',
       author_email='mehdi@mehdix.org',

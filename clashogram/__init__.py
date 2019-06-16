84e237dba42ef73cfcad4ac40c41641c56ebada5
mehdisadeghi <mehdi@mehdix.org>
2019-06-16 20:32:35 +0200

    Add serverless entry.

--- clashogram/__init__.py
+++ clashogram/__init__.py
@@ -1,7 +1,7 @@
 """Clash of Clans war moniting for telegram channels."""
 
-__version__ = '0.5.3'
+__version__ = '0.6.0'
 
-from ._clashogram import main
+from ._clashogram import main, serverless
 
-__all__ = ['main']
+__all__ = ['main', 'serverless']
